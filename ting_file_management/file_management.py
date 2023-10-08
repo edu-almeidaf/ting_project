70a59a3e5cadc0ef082c7c5932b57a4b79d72738
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-10-08 00:22:57 -0300

    feat: Resolvido exercício 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError()
+
+        with open(path_file, 'r') as file:
+            content = file.read().split('\n')
+        return content
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    except ValueError:
+        print("Formato inválido", file=sys.stderr)
