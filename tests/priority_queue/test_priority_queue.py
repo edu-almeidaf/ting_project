3f9994a2f5f85a71bfdccf75cfa07b5528f4cf7f
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-10-08 13:26:35 -0300

    feat: Finalizado Requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,39 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(1000)
+
+    priority_queue.enqueue({"qtd_linhas": 4})
+    assert len(priority_queue.high_priority) == 1
+
+    priority_queue.enqueue({"qtd_linhas": 12})
+    assert len(priority_queue.regular_priority) == 1
+
+    priority_queue.enqueue({"qtd_linhas": 1})
+    assert len(priority_queue.high_priority) == 2
+
+    assert len(priority_queue) == 3
+
+    assert priority_queue.search(0) == {"qtd_linhas": 4}
+    assert priority_queue.search(1) == {"qtd_linhas": 1}
+    assert priority_queue.search(2) == {"qtd_linhas": 12}
+
+    priority_queue.dequeue()
+    assert len(priority_queue.high_priority) == 1
+    assert len(priority_queue.regular_priority) == 1
+    assert len(priority_queue) == 2
+
+    priority_queue.dequeue()
+    assert len(priority_queue.high_priority) == 0
+    assert len(priority_queue.regular_priority) == 1
+    assert len(priority_queue) == 1
+
+    priority_queue.dequeue()
+    assert len(priority_queue.high_priority) == 0
+    assert len(priority_queue.regular_priority) == 0
+    assert len(priority_queue) == 0
